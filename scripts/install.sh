690f8f48c0738b7bcd0efeddd8e72820b350aab6
Benedict Lau <ben.hy.lau@gmail.com>
2016-05-26 01:44:14 -0400

    Fix cjdns install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+set -e
+
 cd /home/pi
 
 # Get tools
 if ! [ "$(which git)" ] || ! [ "$(which nodejs)" ] || ! [ "$(which iperf3)" ]; then
 	sudo apt-get update
 	sudo apt-get install git nodejs iperf3
+fi
+
+# Get cjdns
+if ! [ -d "cjdns" ]; then
+	git clone https://github.com/cjdelisle/cjdns.git
+fi
+cd cjdns
+
+# Build cjdns with optimizations
+if ! [ -x "cjdroute" ]; then
+	./clean
+	git checkout cjdns-v17.3
+	NO_TEST=1 Seccomp_NO=1 CFLAGS="-s -static -Wall -mfpu=neon -mcpu=cortex-a7 -mtune=cortex-a7 -fomit-frame-pointer -marm" ./do
+fi
+
+# Generate cjdns node configurations
+if ! [ -f "cjdroute.conf" ]; then
+	./cjdroute --genconf > cjdroute.conf
 fi
 
 # Download scripts to bring up this mesh node
--- scripts/start_cjdns.sh
+++ scripts/start_cjdns.sh
@@ -1,22 +1,6 @@
 #!/bin/sh
 
-cd /home/pi
-
-# Get cjdns
-if ! [ -d "cjdns" ]; then
-	git clone https://github.com/hyperboria/cjdns.git
-fi
-cd cjdns
-
-# Build cjdns with optimizations
-if ! [ -x "cjdroute" ]; then
-	./clean && NO_TEST=1 Seccomp_NO=1 CFLAGS="-s -static -Wall -mfpu=neon -mcpu=cortex-a7 -mtune=cortex-a7 -fomit-frame-pointer -marm" ./do
-fi
-
-# Generate cjdns node configurations
-if ! [ -f "cjdroute.conf" ]; then
-	./cjdroute --genconf > cjdroute.conf
-fi
+cd /home/pi/cjdns
 
 # Run cjdns
 sudo ./cjdroute < cjdroute.conf
