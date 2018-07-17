10d1901ec37287333d9a6fa38b4b861c7d1d4a4a
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-07-16 22:39:39 -0400

    Missing sudo

--- scripts/ssb-web/ssb-web-broadcast-service.sh
+++ scripts/ssb-web/ssb-web-broadcast-service.sh
@@ -5,7 +5,7 @@ while true; do
     for int in $(ls -1Atu /sys/class/net ); do
       ip=$(ip addr show $int | grep -v inet6 | grep -v '127.0.0.1' |grep inet | head -n 1 | awk '{print $2}' | awk -F "/" '{print $1}')
       if ! [ -z "$ip" ]; then
-        echo -n "net:$ip:8008~shs:$id" |  socat -T 1 - UDP4-DATAGRAM:255.255.255.255:8008,broadcast,so-bindtodevice=$int &        
+        echo -n "net:$ip:8008~shs:$id" | sudo socat -T 1 - UDP4-DATAGRAM:255.255.255.255:8008,broadcast,so-bindtodevice=$int &        
       fi
     done
   done <<< $(sudo cat /var/www/backend/keys/* | grep id | grep -v "#" | awk '{print $2}' | tr -d '"' | sed 's/.ed25519//' | sed 's/@//')
