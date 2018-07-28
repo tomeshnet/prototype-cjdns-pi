b9a528edec3793b9187a615c0356944f6af02d96
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-07-28 18:33:16 -0400

    restore deleted line

--- scripts/ipfs/peer-ipfs-bootstrap/check-and-add-peers.sh
+++ scripts/ipfs/peer-ipfs-bootstrap/check-and-add-peers.sh
@@ -14,7 +14,7 @@ ipfs_peers=$(ipfs swarm peers | wc -l)
 # Checking that there aren't any IPFS peers
 if [[ -z ${ipfs_peers} || ${ipfs_peers} -eq 0 ]]; then
         echo "No IPFS peers were found, looking for them now."
-        
+
         # Were there any peers that were previously saved, but obviously now no longer work?
         if ! [[ $(cat /var/lib/peer-ipfs-boostrap/peers.data | wc -l) -eq 0 || ( $(cat /var/lib/peer-ipfs-boostrap/peers.data | wc -l) -gt 0 && $(head -c 5 /var/lib/peer-ipfs-bootstrap/peers.data) = /ip6/ ) ]]; then
             read -a prev_peers <<< `cat /var/lib/peer-ipfs-bootstrap/peers.data | xargs`
@@ -23,7 +23,7 @@ if [[ -z ${ipfs_peers} || ${ipfs_peers} -eq 0 ]]; then
 
                 # Check if it's online
                 if ping -c 5 $address; then
-                    
+
                     # It's online, check to see if IPFS is enabled
                     res=$(curl ${address}/nodeinfo.json)
                     if [[ echo ${res} | jq -r '.services | contains(["ipfs"])' ]]; then
@@ -52,19 +52,19 @@ if [[ -z ${ipfs_peers} || ${ipfs_peers} -eq 0 ]]; then
         # Get 1-hop cjdns peers to query them
         new_peers=0
         read -a peers <<< `sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($2 == "ESTABLISHED") print $1 }' | awk -F. '{ print $6".k" }' | xargs`
-        
+
         until [[ ${new_peers} -eq ${MAX_PEERS} || ${#peers[@]} -eq 0 ]]; do
-                
+
                 # Reset the list to the next top unchecked peer
                 peer=${peers[0]}
                 peer=$(sudo /opt/cjdns/publictoip6 $peer)
-                
+
                 # See if they have IPFS enabled
                 res=$(curl ${peer}/nodeinfo.json)
                 if [[ echo ${res} | jq -r '.services | contains(["ipfs"])' ]]; then
                         id=$(echo ${res} | jq -r '.services.ipfs.ID')
-                                
-                        # Add them as a bootstrap peer 
+
+                        # Add them as a bootstrap peer
                         ipfs bootstrap add "/ip6/${peer}/tcp/4001/ipfs/${id}"
                         echo "/ip6/${peer}/tcp/4001/ipfs/${id}" >> /var/lib/peer-ipfs-bootstrap/peers.data
                         new_peers=$((new_peers + 1))
@@ -73,11 +73,11 @@ if [[ -z ${ipfs_peers} || ${ipfs_peers} -eq 0 ]]; then
 
                 # Remove them
                 peers=("${peers[@]/$peer}")
-                
+
                 # Add all that node's peers to the bottom of the list to check further hop peers
                 # XXX: The below command hasn't been working -- so for now only 1-hop peers are checked
                 #peers+=$(cjdnstool query getpeers $peer | sed -e '1d;$d' |awk -F. '{ print $6".k" }')
-        
+
         done
         echo "Restarting ipfs.service..."
         sudo systemctl restart ipfs
