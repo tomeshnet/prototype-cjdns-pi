af2f1655e068c512ddb500f2810089c201b37fb0
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-08-12 23:23:19 -0400

    Move install elements in nodeinfo to install

--- scripts/shared/nodeinfo/nodeinfo-update.sh
+++ scripts/shared/nodeinfo/nodeinfo-update.sh
@@ -3,13 +3,7 @@
 cp /opt/tomesh/nodeinfo.json /tmp
 
 # Replace placeholders with dynamic info
-sed -i -e "s/__REPO__/$(git remote get-url origin | awk -F / '{print $5}'| cut -d '.' -f1)/g" /tmp/nodeinfo.json
-sed -i -e "s/__BRANCH__/$(git rev-parse --abbrev-ref HEAD)/g" /tmp/nodeinfo.json
-sed -i -e "s/__COMMIT__/$(git rev-parse HEAD)/g" /tmp/nodeinfo.json
-sed -i -e "s/__INSTALLED__/$(date)/g" /tmp/nodeinfo.json
-
 sed -i -e "s/__KEY__/$(grep -m 1 '"ipv6"' /etc/cjdroute.conf | awk '{ print $2 }' | sed 's/[",]//g')/g" /tmp/nodeinfo.json
-sed -i -e "s/__ORG__/$MESH_NAME/g" /tmp/nodeinfo.json
 
 services=$(run-parts /opt/tomesh/nodeinfo.d/ | sed ':a $!{N; ba}; s/\n/\\n/g')
 services=${services%,*}
