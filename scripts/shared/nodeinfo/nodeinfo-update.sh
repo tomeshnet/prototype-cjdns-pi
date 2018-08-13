b9144d5ddd8de6a162e9ab2ad966207149081feb
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-08-12 23:16:57 -0400

    Remove extra comman and syntax via JQ

--- scripts/shared/nodeinfo/nodeinfo-update.sh
+++ scripts/shared/nodeinfo/nodeinfo-update.sh
@@ -12,6 +12,8 @@ sed -i -e "s/__KEY__/$(grep -m 1 '"ipv6"' /etc/cjdroute.conf | awk '{ print $2 }
 sed -i -e "s/__ORG__/$MESH_NAME/g" /tmp/nodeinfo.json
 
 services=$(run-parts /opt/tomesh/nodeinfo.d/ | sed ':a $!{N; ba}; s/\n/\\n/g')
+services=${services%,*}
+
 sed -i -e "s/__SERVICES__/$services/g" /tmp/nodeinfo.json
 
-sudo cp  /tmp/nodeinfo.json /var/www/html
+cat  /tmp/nodeinfo.json | jq > /var/www/html/nodeinfo.json 
