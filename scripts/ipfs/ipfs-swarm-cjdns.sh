f9cedf097de97d757c23d993da20fc121f8f282f
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-08-14 10:42:38 -0400

    Fixed syntax

--- scripts/ipfs/ipfs-swarm-cjdns.sh
+++ scripts/ipfs/ipfs-swarm-cjdns.sh
@@ -14,17 +14,21 @@ fi
 while read -r cjdns_peer; do
     cjdns_addr=$(sudo /opt/cjdns/publictoip6 $cjdns_peer)
 
-    # See if they have ipfs announced
+    # See if they have IPFS enabled
     res=$(curl http://[${cjdns_addr}]/nodeinfo.json)
     if [ ! -x "$res" ]; then
         id=$(echo ${res} | jq -r -M '.services.ipfs.ID')
         # Value is found
         if [[ ! ${id} == "null" ]] && [[ ! "${id}" == "" ]]; then
-            # Connect ipfs to the peer
+            # Connect to neighbouring ipfs
             ipfs swarm connect "/ip6/${cjdns_addr}/tcp/4001/ipfs/${id}"
-            echo "Connecting to ${cjdns_addr}."
+            echo "Connecting to ${cjdns_addr}"
         fi
     fi
+
+    # Add all that node's peers to the bottom of the list to check further hop peers
+    # XXX: The below command hasn't been working -- so for now only 1-hop peers are checked
+    #peers+=$(cjdnstool query getpeers $peer | sed -e '1d;$d' |awk -F. '{ print $6".k" }')
 
 done <<< `sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($2 == "ESTABLISHED") print $1 }' | awk -F. '{ print $6".k" }' | xargs`
 
