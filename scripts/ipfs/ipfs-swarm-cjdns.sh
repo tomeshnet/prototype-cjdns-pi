5d53c7fce3471ad63fd156179a5c63f57ddae22c
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-08-27 22:45:34 -0400

    requided sudo and silence curl

--- scripts/ipfs/ipfs-swarm-cjdns.sh
+++ scripts/ipfs/ipfs-swarm-cjdns.sh
@@ -2,7 +2,7 @@
 
 # Wait for ipfs to initalize
 attempts=15
-until [[ $(curl http://localhost:5001/api/v0/id -q 2>/dev/null) || ${attempts} -eq 0 ]]; do
+until [[ $(curl http://localhost:5001/api/v0/id -s 2>/dev/null) || ${attempts} -eq 0 ]]; do
     sleep 1
     attempts=$((attempts-1))
 done
@@ -15,7 +15,7 @@ while read -r cjdns_peer; do
     cjdns_addr=$(sudo /opt/cjdns/publictoip6 $cjdns_peer)
 
     # See if they have IPFS enabled
-    res=$(curl http://[${cjdns_addr}]/nodeinfo.json)
+    res=$(curl http://[${cjdns_addr}]/nodeinfo.json -s)
     if [ ! -x "$res" ]; then
         id=$(echo ${res} | jq -r -M '.services.ipfs.ID')
         # Value is found
@@ -33,4 +33,4 @@ while read -r cjdns_peer; do
 done <<< `sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($2 == "ESTABLISHED") print $1 }' | awk -F. '{ print $6".k" }' | xargs`
 
 # Update peers data since ipfs just started
-/usr/local/bin/nodeinfo-update.sh
+sudo /usr/local/bin/nodeinfo-update.sh
