5a948cbfbb3da266ec990afaa8eb86c29bd4d86f
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2018-12-01 15:39:52 -0500

    Corrected spelling mistake

--- scripts/hostapd/nat.sh
+++ scripts/hostapd/nat.sh
@@ -7,6 +7,6 @@ ip6tables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmt
 # Forward all IPv4 traffic from the internal network to the eth0 device and mask with the eth0 external IP address
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-# Allow all IPv6 traffic routed out tun0 an ygg0 to be masked with their respective external IP address
+# Allow all IPv6 traffic routed out tun0 and ygg0 to be masked with their respective external IP address
 ip6tables -t nat -A POSTROUTING -o tun0 -j MASQUERADE
 ip6tables -t nat -A POSTROUTING -o ygg0 -j MASQUERADE
