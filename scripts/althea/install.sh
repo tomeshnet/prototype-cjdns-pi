8a9a79dcafb944e74e219c59631dcb0a4c068b5d
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-01-19 20:04:34 -0500

    Added babeld service information

--- scripts/althea/install.sh
+++ scripts/althea/install.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
+
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Install custom babeld
 wget https://github.com/althea-mesh/babeld/archive/0.1.1.zip
@@ -7,6 +11,12 @@ cd babeld-0.1.1
 make
 sudo cp babeld /usr/bin/babeld
 cd ..
+
+sudo cp "$BASE_DIR/babeld.conf" "/etc/babeld.conf"
+sudo cp "$BASE_DIR/babeld.service" /etc/systemd/system/babeld.service
+sudo systemctl daemon-reload
+sudo systemctl enable babeld.service
+sudo systemctl start babeld.service
 
 # Install wireguard  
 sudo apt-get install libmnl-dev libelf-dev build-essential pkg-config raspberrypi-kernel-headers
