439a60e05dccc21efa54d8ece9c0ef3e07fea40f
makeworld <makeworld@protonmail.com>
2019-02-08 20:05:13 -0500

    Made script a loop

--- scripts/ipfs/ipfs-setup.sh
+++ scripts/ipfs/ipfs-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# This script sets up IPFS.
-# It connects to local mesh peers,
+# This script connects to local mesh peers,
 # and it sets up connection filters based on what networks this node can access.
+# It runs continually, to change IPFS settings as the environment around the node changes.
 
 
 # Wait for IPFS to initalize
@@ -17,9 +17,9 @@ if [[  ${attempts} -eq 0 ]]; then
     exit 1
 fi
 
-#################################
-# Connect to local mesh peers
-#################################
+# Update peers's data since IPFS just started
+echo "Updating nodeinfo..."
+sudo /usr/local/bin/nodeinfo-update.sh
 
 function addPeer  {
     addr=$1
@@ -41,62 +41,55 @@ function addPeer  {
     fi
 }
 
-# Add CJDNS direct peers if the service is running
-if [ "$(systemctl status cjdns.service | grep 'Active: ' | awk '{ print $2 }')" = 'active' ]; then
-    while read -r cjdns_peer; do
-        cjdns_addr=$(sudo /opt/cjdns/publictoip6 "$cjdns_peer")
-        addPeer "${cjdns_addr}"
+# Continually mesh with peers and add or remove filter rules as networks go up and down
+while true; do
+    # Check if CJDNS is running
+    if [ "$(systemctl status cjdns.service | grep 'Active: ' | awk '{ print $2 }')" = 'active' ]; then
+        # Connect with direct peers over IPFS
+        while read -r cjdns_peer; do
+            cjdns_addr=$(sudo /opt/cjdns/publictoip6 "$cjdns_peer")
+            addPeer "${cjdns_addr}"
 
-        # Add all that node's peers to the bottom of the list to check further hop peers
-        # XXX: The below command hasn't been working -- so for now only 1-hop peers are checked
-        #peers+=$(cjdnstool query getpeers $peer | sed -e '1d;$d' |awk -F. '{ print $6".k" }')
+            # Add all that node's peers to the bottom of the list to check further hop peers
+            # XXX: The below command hasn't been working -- so for now only 1-hop peers are checked
+            #peers+=$(cjdnstool query getpeers $peer | sed -e '1d;$d' |awk -F. '{ print $6".k" }')
 
-    done <<< "$(sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($3 == "ESTABLISHED") print $2 }' | awk -F. '{ print $6".k" }' | xargs)"
-fi
+        done <<< "$(sudo nodejs /opt/cjdns/tools/peerStats 2>/dev/null | awk '{ if ($3 == "ESTABLISHED") print $2 }' | awk -F. '{ print $6".k" }' | xargs)"
 
-# Add Yggdrasil direct peers if the service is running
-if [ "$(systemctl status yggdrasil.service | grep 'Active: ' | awk '{ print $2 }')" = 'active' ]; then
-    while read -r ygg_peer; do
-        addPeer "${ygg_peer}"
-    done <<< "$(sudo yggdrasilctl getPeers | grep -v "(self)" | awk '{print $1}' | grep -v bytes_recvd | xargs)"
-fi
+        # Remove a filter if there was one
+        ipfs swarm filters rm '/ip6/fc00::/ipcidr/8'
+    else
+        ipfs swarm filters add '/ip6/fc00::/ipcidr/8'
+        echo "Blocked CJDNS network"
+    fi
 
-#################################
-# Setup connection filter rules
-#################################
+    if [ "$(systemctl status yggdrasil.service | grep 'Active: ' | awk '{ print $2 }')" = 'active' ]; then
+        while read -r ygg_peer; do
+            addPeer "${ygg_peer}"
+        done <<< "$(sudo yggdrasilctl getPeers | grep -v "(self)" | awk '{print $1}' | grep -v bytes_recvd | xargs)"
 
-# Remove previous ones first, just in case the script is run more than once
-ipfs swarm filters rm '/ip6/fc00::/ipcidr/8'
-ipfs swarm filters rm '/ip6/0200::/ipcidr/7'
-ipfs swarm filters rm '/ip4/0.0.0.0/ipcidr/0'
-ipfs swarm filters rm '/ip6/2000::/ipcidr/3'
+        ipfs swarm filters rm '/ip6/0200::/ipcidr/7'
+    else
+        ipfs swarm filters add '/ip6/0200::/ipcidr/7'
+        echo "Blocked Yggdrasil network"
+    fi
 
-# If CJDNS isn't running...
-if ! [ "$(systemctl status cjdns.service | grep 'Active: ' | awk '{ print $2 }')" = 'active' ]; then
-    # Block connecting to CJDNS nodes
-    ipfs swarm filters add '/ip6/fc00::/ipcidr/8'
-    echo "Blocked CJDNS network"
-fi
-# Yggdrasil
-if ! [ "$(systemctl status yggdrasil.service | grep 'Active: ' | awk '{ print $2 }')" = 'active' ]; then
-    ipfs swarm filters add '/ip6/0200::/ipcidr/7'
-    echo "Blocked Yggdrasil network"
-fi
-# Clearnet or regular Internet IPv4 access
-if ! ping -w 2 1.1.1.1 &>/dev/null; then
-    # Block all IPv4 - it's not used anywhere else
-    ipfs swarm filters add '/ip4/0.0.0.0/ipcidr/0'
-    echo "Blocked all of IPv4"
-fi
-# Clearnet IPv6 access
-if ! ping -w 2 2606:4700:4700::1111 &>/dev/null; then  # IPv6 version of 1.1.1.1 above
-    # Block global unicast for IPv6
-    ipfs swarm filters add '/ip6/2000::/ipcidr/3'
-    echo "Blocked IPv6 internet"
-fi
+    # Clearnet or regular Internet IPv4 access
+    if ! ping -w 2 1.1.1.1 &>/dev/null; then
+        # Block all IPv4 - it's not used anywhere else
+        ipfs swarm filters add '/ip4/0.0.0.0/ipcidr/0'
+        echo "Blocked all of IPv4"
+    else
+        ipfs swarm filters rm '/ip4/0.0.0.0/ipcidr/0'
+    fi
 
-# Update peers data since ipfs just started
-echo "Updating nodeinfo..."
-sudo /usr/local/bin/nodeinfo-update.sh
+    # Clearnet IPv6 access
+    if ! ping -w 2 2606:4700:4700::1111 &>/dev/null; then  # IPv6 version of 1.1.1.1 above
+        # Block global unicast for IPv6
+        ipfs swarm filters add '/ip6/2000::/ipcidr/3'
+        echo "Blocked IPv6 internet"
+    else
+        ipfs swarm filters rm '/ip6/2000::/ipcidr/3'
+    fi
 
-echo -e "\n\nScript complete."
+done
