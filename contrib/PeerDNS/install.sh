f1341b7ecd96d8154dd8858888747019f3ba8839
makeworld <makeworld@protonmail.com>
2019-02-21 20:54:54 -0500

    echo -> echo -e

--- contrib/PeerDNS/install.sh
+++ contrib/PeerDNS/install.sh
@@ -8,36 +8,37 @@ ELIXIR_VERSION=1.8.1
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if [ "$(whoami)" == root ]; then
-    echo "\033[1;36mDo not run this script as root. It will install things globally on its own."
+    echo -e "\033[1;36mDo not run this script as root. It will install things globally on its own."
     exit 1
 fi
 
 cd /tmp
-echo "\033[1;36mInstalling Erlang $ERLANG_VERSION ..."
+echo -e "\033[1;36mInstalling Erlang $ERLANG_VERSION ..."
 wget "https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_$ERLANG_VERSION-1~raspbian~stretch_armhf.deb"
 sudo dpkg -i "esl-erlang_$ERLANG_VERSION-1~raspbian~stretch_armhf.deb" || true
 sudo apt install -f -y  # Fix required deps
-echo "\033[1;36mErlang installed."
+echo -e "\033[1;36mErlang installed."
 
-echo "\033[1;36mInstalling Elixir $ELIXIR_VERSION ..."
+echo -e "\033[1;36mInstalling Elixir $ELIXIR_VERSION ..."
 sudo mkdir /opt/elixir || true
 sudo chmod -R 755 /opt/elixir  # Everyone can read and execute - only root can write
 sudo wget -P /opt/elixir "https://github.com/elixir-lang/elixir/releases/download/v$ELIXIR_VERSION/Precompiled.zip"
 sudo unzip /opt/elixir/Precompiled.zip -d /opt/elixir
 sudo rm /opt/elixir/Precompiled.zip
-echo 'export PATH="/opt/elixir/bin:$PATH"' | sudo tee -a /etc/profile  # Add to elixir binaries to PATH
-echo "\033[1;36mElixir installed."
+echo -e 'export PATH="/opt/elixir/bin:$PATH"' | sudo tee -a /etc/profile  # Add to elixir binaries to PATH
+export PATH="/opt/elixir/bin:$PATH"
+echo -e "\033[1;36mElixir installed."
 
-echo "\033[1;36mInstalling libsodium from stretch-backports..."
+echo -e "\033[1;36mInstalling libsodium from stretch-backports..."
 # Debian keys needed for stretch-backports to be enabled, but Raspbian won't let you download them through apt
 wget -P /tmp http://ftp.ca.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2017.5_all.deb
 sudo dpkg -i /tmp/debian-archive-keyring_2017.5_all.deb
-echo "deb http://ftp.ca.debian.org/debian stretch-backports main" | sudo tee -a /etc/apt/sources.list
+echo -e "deb http://ftp.ca.debian.org/debian stretch-backports main" | sudo tee -a /etc/apt/sources.list
 sudo apt update -y
 sudo apt install -y -t stretch-backports libsodium23 libsodium-dev
-echo "\033[1;36mlibsodium from stretch-backports installed."
+echo -e "\033[1;36mlibsodium from stretch-backports installed."
 
-echo "\033[1;36mInstalling PeerDNS in /opt ..."
+echo -e "\033[1;36mInstalling PeerDNS in /opt ..."
 cd /opt
 sudo git clone https://github.com/p2pstuff/PeerDNS.git
 sudo chmod -R 777 PeerDNS  # XXX: Maybe having everyone be able to write is bad?
@@ -53,13 +54,13 @@ cd ..
 # Copy over config file
 cp "$BASE_DIR/config.exs" config/
 # Setup dnsmasq to resolve those tlds
-echo "address=/p2p/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
-echo "address=/mesh/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
-echo "address=/h/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
-echo "address=/hype/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
-echo "address=/y/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
-echo "address=/ygg/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
-echo "address=/tomesh/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/p2p/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/mesh/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/h/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/hype/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/y/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/ygg/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
+echo -e "address=/tomesh/127.0.0.1#5454" | sudo tee -a /etc/dnsmasq.conf
 sudo systemctl restart dnsmasq
 
 sudo iptables -A INPUT -j ACCEPT -p tcp --dport 14123
@@ -70,14 +71,14 @@ sudo ip6tables -A INPUT -j ACCEPT -p udp --dport 14123
 cp "$BASE_DIR/peerdns.service" /usr/local/lib/systemd/system
 sudo systemctl daemon-reload
 
-echo "\033[1;36mPeerDNS installed and set up, in /opt."
-echo ""
-echo "\033[1;36mThe firewall has been opened temporarily. To keep the required ports open after a reboot, add these lines to /etc/iptables/rules.v4 and /etc/iptables/rules.v6, in the raw INPUT rules section."
-echo "\033[0;36m    -A INPUT -j ACCEPT -p tcp --dport 14123"
-echo "\033[0;36m    -A INPUT -j ACCEPT -p udp --dport 14123"
-echo ""
-echo "\033[1;36mRun \033[0;36msudo systemctl start peerdns.service\033[1;36m to start PeerDNS."
-echo "\033[1;36mRun \033[0;36msudo systemctl enable peerdns.service\033[1;36m to start it at boot everytime."
-echo "\033[1;36m CJDNS peers will automatically be peered with."
-echo "\033[1;36mAdd Yggdrasil peers permanently (for now) by running the ygg-peer.sh script in this folder."
-echo "\033[1;36mNavigate to this device at port 14123 in your browser for a web UI to add domain records."
+echo -e "\033[1;36mPeerDNS installed and set up, in /opt."
+echo -e ""
+echo -e "\033[1;36mThe firewall has been opened temporarily. To keep the required ports open after a reboot, add these lines to /etc/iptables/rules.v4 and /etc/iptables/rules.v6, in the raw INPUT rules section."
+echo -e "\033[0;36m    -A INPUT -j ACCEPT -p tcp --dport 14123"
+echo -e "\033[0;36m    -A INPUT -j ACCEPT -p udp --dport 14123"
+echo -e ""
+echo -e "\033[1;36mRun \033[0;36msudo systemctl start peerdns.service\033[1;36m to start PeerDNS."
+echo -e "\033[1;36mRun \033[0;36msudo systemctl enable peerdns.service\033[1;36m to start it at boot everytime."
+echo -e "\033[1;36m CJDNS peers will automatically be peered with."
+echo -e "\033[1;36mAdd Yggdrasil peers permanently (for now) by running the ygg-peer.sh script in this folder."
+echo -e "\033[1;36mNavigate to this device at port 14123 in your browser for a web UI to add domain records."
--- contrib/PeerDNS/uninstall.sh
+++ contrib/PeerDNS/uninstall.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 if [ "$(whoami)" == root ]; then
-    echo "\033[1;36mDo not run this script as root. It will install things globally on its own."
+    echo -e "\033[1;36mDo not run this script as root. It will install things globally on its own."
     exit 1
 fi
 
-echo "\033[1;36mUninstalling Erlang..."
+echo -e "\033[1;36mUninstalling Erlang..."
 sudo dpkg -r esl-erlang
-echo "\033[1;36mErlang uninstalled."
+echo -e "\033[1;36mErlang uninstalled."
 
-echo "\033[1;36mUninstalling Elixir..."
+echo -e "\033[1;36mUninstalling Elixir..."
 sudo rm -rf /opt/elixir
 sudo sed -i 's/export PATH="\/opt\/elixir\/bin:$PATH"//' /etc/profile
-echo "\033[1;36mElixir uninstalled."
+echo -e "\033[1;36mElixir uninstalled."
 
-echo "\033[1;36mUninstalling libsodium..."
+echo -e "\033[1;36mUninstalling libsodium..."
 sudo dpkg -r debian-archive-keyring
 sudo apt remove libsodium23 libsodium-dev
 sudo sed -i 's/deb http:\/\/ftp.ca.debian.org\/debian stretch-backports main//' /etc/apt/sources.list
 sudo apt update
-echo "\033[1;36mlibsodium from stretch-backports uninstalled."
+echo -e "\033[1;36mlibsodium from stretch-backports uninstalled."
 
-echo "\033[1;36mUninstalling PeerDNS..."
+echo -e "\033[1;36mUninstalling PeerDNS..."
 # Remove all dnsmasq redirecting lines
 sudo sed -i 's/address=\/.*\/127.0.0.1#5454//g' /etc/dnsmasq.conf
 rm -rf "/opt/PeerDNS"
-echo "\033[1;36mPeerDNS uninstalled."
-echo "\033[1;36mRemember to remove any rules in /etc/iptables mentioning port 14123."
+echo -e "\033[1;36mPeerDNS uninstalled."
+echo -e "\033[1;36mRemember to remove any rules in /etc/iptables mentioning port 14123."
+echo -e "\033[1;36mI would suggest running \033[0;36msudo apt autoremove\033[1;36mto remove dependencies."
