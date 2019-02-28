33ff20a673fbb04ca0a099b8be15d2032c54d123
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-02-27 21:23:42 -0500

    NodeExporter ygg rx/tx (#311)
    * Added yggdrasil peers report
    
    * missing json import
    
    * Remove sudo (already running as root)
    
    * Added cjdns peer information
    
    * Nodeexporter cjdns
    
    * Added cjdns keys from peerStats mac addresses for better linking for nodes
    s (#277)
    * correced inverted quotes
    
    * Corrected formatting
    
    * rollback of old code

--- scripts/prometheus-node-exporter/nodestats-tomesh.py
+++ scripts/prometheus-node-exporter/nodestats-tomesh.py
@@ -3,6 +3,7 @@ import os.path
 import time
 import shlex
 import subprocess
+import json
 
 path = "/var/lib/node_exporter/ne-stats.prom"
 
@@ -14,7 +15,7 @@ os.mkfifo(path)
 while 1:
         fifo = open(path, "w")
 
-        fifo.write("tomeshV 1.1\n")
+        fifo.write("tomeshV 1.2\n")
 
         s=""
         if os.path.isfile("/sys/devices/virtual/thermal/thermal_zone0/temp"):
@@ -22,6 +23,35 @@ while 1:
                     s = f.read()
                 fifo.write("hw_temp ")
                 fifo.write(s)
+
+
+        #Wireless Link Dump
+
+        # Get cjdns peer information
+        remotePeers = {}
+        myaddress=""
+        if os.path.isfile("/opt/cjdns/tools/cexec"):
+            command_line = "/opt/cjdns/tools/cexec \"Core_nodeInfo()\""
+            args = shlex.split(command_line)
+            interfaces = subprocess.Popen(args,stdout=subprocess.PIPE)
+            interfaces.wait()
+            output = interfaces.stdout.read();
+            data = json.loads(output)
+            tmp=data["myAddr"].split(".")
+            myaddress=tmp[5] + ".k"
+            myaddress=myaddress.splitlines()[0]
+            command_line = "/opt/cjdns/tools/peerStats"
+            args = shlex.split(command_line)
+            interface = subprocess.Popen(args,stdout=subprocess.PIPE)
+            interface.wait()
+            output = interface.stdout.read()
+            peers = output.split("\n")
+            for peer in peers:
+                words2= peer.split(" ")
+                if len(words2) > 1:
+                        tmp=words2[1].split(".")
+                        remotePubKey=tmp[5] + ".k"
+                        remotePeers[words2[0]]=remotePubKey
 
         # Look through wireless interfaces
         command_line = "iw dev"
@@ -72,6 +102,9 @@ while 1:
                                                     rx=-1
                                                     tx=-1
                                                     signal=-100
+                                                    cjdnsdata=""
+                                                    if station in remotePeers:
+                                                        cjdnsdata=',sourcekey="' + myaddress + '", key="' + remotePeers[station] + '"'
                                                 if words3[1].find("signal") > -1:
                                                     signal=words3[3]
                                                 if words3[1].find("mesh") > -1 and words3[2].find("plink") > -1:
@@ -81,9 +114,21 @@ while 1:
                                                 if words3[1].find("tx") > -1 and words3[2].find("bytes") > -1:
                                                        tx=words3[3]
                                                 if words3[1].find("TDLS") > -1:
-                                                       fifo.write('mesh_node_signal{sourcemac="' + mac + '",mac="' + station + '",link="' + linkstatus + '"} ' + signal + "\n")
+                                                       fifo.write('mesh_node_signal{sourcemac="' + mac + '",mac="' + station + '",link="' + linkstatus + '"' + cjdnsdata + '} ' + signal + "\n")
                                                        fifo.write('mesh_node_rx{sourcemac="' + mac + '",mac="' + station + '"} ' + rx + "\n")
                                                        fifo.write('mesh_node_tx{sourcemac="' + mac + '",mac="' + station + '"} ' + tx + "\n")
+
+
+        if os.path.isfile("/usr/bin/yggdrasilctl"):
+            args = shlex.split("sudo /usr/bin/yggdrasilctl -json getPeers")
+            interfaces = subprocess.Popen(args,stdout=subprocess.PIPE)
+            interfaces.wait()
+            raw_json = interfaces.stdout.read();
+            peers = json.loads(raw_json.decode())
+
+            for peer,data in peers["peers"].items():
+                fifo.write('mesh_node_ygg_peer_rx{peer="'+peer+'",endpoint="'+str(data["endpoint"])+'"}'+" "+str(data["bytes_recvd"])+"\n")
+                fifo.write('mesh_node_ygg_peer_tx{peer="'+peer+'",endpoint="'+str(data["endpoint"])+'"}'+" "+str(data["bytes_sent"])+"\n")
 
         fifo.close()
         time.sleep(1)
