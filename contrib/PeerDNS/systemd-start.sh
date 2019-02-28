924774829856562517cb644e81de4b6dc5a4895d
makeworld <makeworld@protonmail.com>
2019-02-28 17:26:28 -0500

    Fixed systemd file and added pi to yggdrasil group

--- contrib/PeerDNS/systemd-start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo -e "\n\n" >> /var/log/PeerDNS.log
-cd /opt/PeerDNS
-mix run --no-halt >> /var/log/PeerDNS.log 2>&1
--- contrib/PeerDNS/systemd-stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ps aux | grep 'mix run --no-halt' | head -n 1 | awk '{ print $2 }' | xargs sudo kill
