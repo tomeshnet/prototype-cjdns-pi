957e87607a6f09de9b740e0adde341a1aaecd685
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-03-10 19:33:46 -0400

    New board detection routine

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2034	
+# shellcheck disable=SC2034
 true
 
 dialogGlobalParams="--backtitle Installation --ascii-lines"
@@ -134,4 +134,66 @@ function askSelection {
         done
         dialogREPLY="$REPLY"
     fi
+}
+
+function detectBoard {
+
+    BOARD_MODEL="Unknown"
+    BOARD_FAMILY="Unknown"
+    BOARD_OS="Unknown"
+
+    # Check for x86
+    ARCH="$(uname -m)"
+    case "$ARCH" in
+    x86_64)
+        BOARD_MODEL="amd64"
+        BOARD_FAMILY="AMD64"
+    ;;
+    i386)
+        BOARD_MODEL="i386"
+        BOARD_FAMILY="i386"
+    esac
+
+    if [[ -z "${BOARD_MODEL}" ]]; then
+        # Use tr to remove null byte generating warning
+        if [ -f "/sys/firmware/devicetree/base/model" ]; then
+            BOARD_MODEL=$(tr -d '\0' < /sys/firmware/devicetree/base/model)
+        else
+            BOARD_MODEL=$(grep Hardware /proc/cpuinfo | awk '{print $3}' | head -n 1)
+        fi
+    fi
+
+    # Check for armbian identification
+    if [ -f "/etc/armbian-image-release " ]; then
+        BOARD_OS="Armbian"
+        BOARD_MODEL="$(grep "BOARD=" /etc/armbian-image-release | awk -F '=' '{print $2}' | tr -d \")"
+        BOARD_NAME="$(grep BOARD_NAME /etc/armbian-image-release | awk -F '=' '{print $2}' | tr -d \" )"
+        BOARD_NEON=true
+    fi
+
+    if [[ "$BOARD_MODEL" == "Raspberry Pi"* ]]; then
+        BOARD_OS="Raspbian"
+
+        # Check for default password is still set for user pi
+        # If it is force password before reboot
+        # shellcheck disable=SC2016
+        if [[ "$BOARD_MODEL" == "Raspberry Pi 3"* ]]; then
+            BOARD_MODEL="Raspberry Pi 3"
+            BOARD_NEON=true
+        fi
+        if [[ "$BOARD_MODEL" == "Raspberry Pi 2"* ]]; then
+            BOARD_MODEL="Raspberry Pi 2"
+            BOARD_NEON=true
+        fi
+        if [[ "$BOARD_MODEL" == "Raspberry Pi A"* || "$BOARD_MODEL" == "Raspberry Pi B"* ]]; then
+            BOARD_MODEL="Raspberry Pi 1"
+            BOARD_NEON=false
+        fi
+
+        if [[ "$BOARD_MODEL" == "Zero"* ]]; then
+            BOARD_MODEL="Raspberry Pi Zero"
+            BOARD_NEON=false
+        fi
+    fi
+
 }
