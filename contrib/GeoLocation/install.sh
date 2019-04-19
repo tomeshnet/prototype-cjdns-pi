0a72923e1d5eb70c0fcd65c0dbfd99eb0d3c0321
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-04-18 21:27:41 -0400

    Update install.sh

--- contrib/GeoLocation/install.sh
+++ contrib/GeoLocation/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get install -y jq
-sudo cp "nodeinfo-geolocation" /opt/tomesh/nodeinfo.d/00_geolocation
-sudo chmod +x /opt/tomesh/nodeinfo.d/00_geolocation
+sudo cp "nodeinfo-geolocation" /opt/tomesh/nodeinfo.d/geolocation
+sudo chmod +x /opt/tomesh/nodeinfo.d/geolocation
