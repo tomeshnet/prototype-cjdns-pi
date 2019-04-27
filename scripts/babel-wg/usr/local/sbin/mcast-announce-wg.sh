e7535ce80da6c4e6e2310cb646bada8128dc87da
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-04-27 00:01:11 -0400

    Create mcast-announce-wg.sh

---
+++ scripts/babel-wg/usr/local/sbin/mcast-announce-wg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for int in $(find /sys/class/net/* -maxdepth 1 -print0 | xargs -0 -l basename); do
+  if [[ "$int" == "eth*" ]] then
+    echo "WG|$publicKey" |  socat - UDP6-datagram:[ff02::1%$int]:1234
+  fi
+  if [[ "$int" == "wlan*" ]] then
+    echo "WG|$publicKey" |  socat - UDP6-datagram:[ff02::1%$int]:1234
+  fi
+done
