50696402c58e2a34279088e858f819de5071931e
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-04-26 23:47:50 -0400

    Create process-wg.sh

---
+++ scripts/babel-wg/process-wg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+SOCAT_PEERADDR=$(echo "$SOCAT_PEERADDR" | tr -d \[ | tr -d \]) #Remote
+SOCAT_SOCKADDR=$(echo "$SOCAT_SOCKADDR" | tr -d \[ | tr -d \]) #Local
+LOCAL=$(echo $SOCAT_SOCKADDR  | tr -d \[ | tr -d \] | tr -d \:)
+IFACE=$(cat /proc/net/if_inet6  | grep $LOCAL  | awk '{print $6}')
+#echo $SOCAT_SOCKADDR
+#/proc/net/if_inet6
+
+if [[ "$SOCAT_SOCKADDR" == "$SOCAT_PEERADDR" ]]; then
+   exit 0
+fi
+
+read -r line
+A="$(cut -d'|' -f1 <<<"$line")"
+if [[ "$A" == "WG" ]]; then
+    peerPub="$(cut -d'|' -f2 <<<"$line")"
+    if [ -z "$(wg 2>&1  |  grep "${peerPub}")" ]; then
+        key=$(echo $peerPub | sha512sum  | awk '{print $1}' | sha512sum | awk '{print $1}')
+        ipv6=0${key:0:31}
+        ip=${ipv6:0:4}:${ipv6:4:4}:${ipv6:8:4}:${ipv6:12:4}:${ipv6:16:4}:${ipv6:20:4}:${ipv6:24:4}:${ipv6:28:4}
+        ip="$(echo $ip | cut -d ":" -f5-8)"
+        wg set wg0 listen-port 1010 private-key key peer $peerPub endpoint [$SOCAT_PEERADDR%$IFACE]:1010 persistent-keepalive 60 allowed-ips fe80::${ip}/128,ff02:0:0:0:0:0:1:6/128
+    fi
+fi
