17a7060823f0b6bd2b8978e21bfcfd4d906eb569
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-05-05 00:09:06 -0400

    Update babel-wg-init.sh

--- scripts/babel-wg/babel-wg-init.sh
+++ scripts/babel-wg/babel-wg-init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+source /etc/wg
 ip link del dev wg0 type wireguard || true
 ip link add dev wg0 type wireguard
 ip link set dev wg0 up
@@ -10,5 +11,4 @@ if [ -z "$(ip addr show dev wg0  | grep inet6\ fe)" ]; then
 	  ip address add dev wg0 scope link fe80::${ip}/64
 fi
 
-source /etc/wg
 ip address add dev wg0 scope link $ipv6/12
