66ad2f1f612469daa3512b47462b5348c0acf8df
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-05-04 23:23:11 -0400

    added  listen-port 1010

--- scripts/babel-wg/babel-wg-init.sh
+++ scripts/babel-wg/babel-wg-init.sh
@@ -3,12 +3,12 @@
 ip link del dev wg0 type wireguard || true
 ip link add dev wg0 type wireguard
 ip link set dev wg0 up
-
+wg set wg0 listen-port 1010
+ 
 if [ -z "$(ip addr show dev wg0  | grep inet6\ fe)" ]; then
 	  ip="$(echo $ipv6 | cut -d ":" -f5-8)"
 	  ip address add dev wg0 scope link fe80::${ip}/64
 fi
 
 source /etc/wg
-
 ip address add dev wg0 scope link $ipv6/12
