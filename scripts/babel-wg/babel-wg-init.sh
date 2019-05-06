1ce7fdeddd48ea45d8130c730ea8caf221d214ad
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-05-06 13:07:10 -0400

    Update babel-wg-init.sh

--- scripts/babel-wg/babel-wg-init.sh
+++ scripts/babel-wg/babel-wg-init.sh
@@ -6,7 +6,6 @@ for int in $(find /sys/class/net/* -maxdepth 1 -print0 | xargs -0 -l basename); 
     ip link del dev $int type wireguard
   fi
 done
-ip -6 address add dev lo scope link $ipv6/12
 
 mkdir -p /var/run/babel-wg
 echo 0 > /var/run/babel-wg/index
