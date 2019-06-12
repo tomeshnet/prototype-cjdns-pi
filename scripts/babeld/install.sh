269f03a63f38a4eb16bf2c40372222ecf552c2dd
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-12 16:07:01 -0400

    Install package

--- scripts/babeld/install.sh
+++ scripts/babeld/install.sh
@@ -2,6 +2,7 @@
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+# Prep working directory
 mkdir /tmp/babeld
 mkdir /tmp/babeld/root
 mkdir /tmp/babeld/src
@@ -9,7 +10,7 @@ mkdir /tmp/babeld/src
 # Prepare root directory
 cp -R $BASE_DIR/files/* /tmp/babeld/root/
 
-# Compile and install babeld into root directory
+# Compile babeld and install babeld into root directory
 last=`pwd`
 git clone git://github.com/jech/babeld.git /tmp/babeld/src
 cd /tmp/babeld/src
@@ -17,8 +18,14 @@ sed -i 's|PREFIX = /usr/local|PREFIX = /tmp/babeld/root|' Makefile
 make
 make install
 cd ..
+
+# Make deb pacakges
 version="$(root/bin/babeld -V  2>&1)"
 version=${version:7}
 echo "Version: $version" >> root/DEBIAN/control
 echo "Architecture: $( dpkg --print-architecture)" >> root/DEBIAN/control
 dpkg-deb --build root
+
+# Install and cleanup
+sudo dpkg -i /tmp/babeld/root.dev
+rm -rf /tmp/babeld
