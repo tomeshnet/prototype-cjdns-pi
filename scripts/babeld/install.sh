ce8b13b4d59da200db2ca0442ad0d361406d4dc3
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-12 15:37:57 -0400

    install.sh

--- scripts/babeld/install.sh
+++ scripts/babeld/install.sh
@@ -2,14 +2,14 @@
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-
 mkdir /tmp/babeld
 mkdir /tmp/babeld/root
 mkdir /tmp/babeld/src
 
-
+# Prepare root directory
 cp -R $BASE_DIR/files /tmp/babeld/root
 
+# Compile and install babeld into root directory
 last=`pwd`
 git clone git://github.com/jech/babeld.git /tmp/babeld/src
 cd /tmp/babeld/src
@@ -17,3 +17,7 @@ sed -i 's|PREFIX = /usr/local|PREFIX = /tmp/babeld/root|' Makefile
 make
 make install
 cd $last
+
+# Copy files over
+cp -R /tmp/babeld/root/* /
+rm -rf /tmp/babeld
