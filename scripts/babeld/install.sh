044718f20038334537f99d48196ebca33ec4f7ae
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-16 08:53:15 -0400

    Update install.sh

--- scripts/babeld/install.sh
+++ scripts/babeld/install.sh
@@ -7,3 +7,10 @@ echo deb deb http://meshwithme.online/deb/repos/apt/debian stretch main | sudo t
 
 sudo apt-get update
 sudo apt-get install babeld
+
+# Dont announce yggdrasil of cjdns addresses
+sudo systemctl stop babled
+echo redistribute deny local ip 200::/7 | sudo tee --append /etc/babeld.conf
+echo redistribute deny local ip 300::/7 | sudo tee --append /etc/babeld.conf
+echo redistribute deny local ip fc00::/8 | sudo tee --append /etc/babeld.conf
+sudo systemctl start babled
