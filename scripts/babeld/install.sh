b93fd5fded5f83f405efe2de781f71b1649b681d
darkdrgn2k <darkdrgn2k@users.noreply.github.com>
2019-06-16 08:47:10 -0400

    corrected repo name

--- scripts/babeld/install.sh
+++ scripts/babeld/install.sh
@@ -3,7 +3,7 @@
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 #Enable test repo
-echo deb http://meshwithme.online/deb/repos Stretch main | sudo tee /etc/apt/sources.list.d/tomesh.list
+echo deb deb http://meshwithme.online/deb/repos/apt/debian stretch main | sudo tee /etc/apt/sources.list.d/tomesh.list
 
 sudo apt-get update
 sudo apt-get install babeld
